373cc99d99257072712f7f168c62ffbacd4cd7e5
raytargosz <raytargosz15@gmail.com>
2023-03-22 21:34:11 -0400

    3.22.23/7
    Check-in - general

--- Assets/Scripts/CombinedPlayerController.cs
+++ Assets/Scripts/CombinedPlayerController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class CombinedPlayerController : MonoBehaviour
 {
     public Camera mainCamera;
@@ -61,6 +62,32 @@ public class CombinedPlayerController : MonoBehaviour
         controller.Move((moveDirection + new Vector3(0, bobbingObjectVerticalMovement, 0)) * Time.deltaTime);
     }
 
+    void FixedUpdate()
+    {
+        FixedUpdateActions();
+    }
+
+    void FixedUpdateActions()
+    {
+        float bobbingObjectVerticalMovement = GetBobbingObjectVerticalMovement();
+
+        UpdateMoveDirection();
+        UpdateFallingDeath();
+        UpdateGroundedStatus();
+        UpdateJump();
+
+        controller.Move((moveDirection + new Vector3(0, bobbingObjectVerticalMovement, 0)) * Time.fixedDeltaTime);
+    }
+
+    private void UpdateJumpInput()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpTime = maxJumpTime;
+        }
+    }
+
+
     private float GetBobbingObjectVerticalMovement()
     {
         return (isGrounded && bobbingObject != null && IsStandingOnBobbingObject())
@@ -83,12 +110,13 @@ public class CombinedPlayerController : MonoBehaviour
         moveDirection.z = relativeDirection.z * currentSpeed;
     }
 
+
     private void UpdateFallingDeath()
     {
         if (!isGrounded && moveDirection.y < 0)
         {
             currentFallingTime += Time.deltaTime;
-            if (currentFallingTime >= maxFallingTime)
+            if (currentFallingTime >= maxFallingTime || Mathf.Approximately(currentFallingTime, maxFallingTime))
             {
                 playerDeath.TriggerDeathSequence();
             }
